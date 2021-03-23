3d7a2d192f063af3224ec712f9aa7899c2b2ffca
yongsannnn <yong_s@live.com>
2021-03-23 03:55:33 +0000

    Added until update

--- route/products.js
+++ route/products.js
@@ -59,4 +59,53 @@ router.post("/create", (req,res)=>{
 })
 
 
+router.get("/:product_id/update", async(req,res)=>{
+    // STEP 1 - Get the product that we want to update
+    // This is the same as "SELEC * From Products WHERE id = ${product_id}"
+    const productToEdit = await Product.where({
+        "id": req.params.product_id
+    }).fetch({
+        required:true
+    });
+    // res.send(productToEdit)
+    
+    // STEP 2 - send the product to the view
+
+    const form = createProductForm();
+    form.fields.name.value = productToEdit.get("name");
+    form.fields.cost.value = productToEdit.get("cost");
+    form.fields.description.value = productToEdit.get("description");
+
+    res.render("products/update", {
+        "form": form.toHTML(bootstrapField),
+        "product": productToEdit.toJSON()
+    })
+})
+
+
+router.post("/:product_id/update", async(req,res)=>{
+    const productToEdit = await Product.where({
+        "id": req.params.product_id
+    }).fetch({
+        required:true
+    });
+
+    const productForm = createProductForm();
+
+    // This shortcut will only work when the name of the table is the same as the name of the value
+    productForm.handle(req,{
+        "success": async(form) =>{
+            productToEdit.set(form.data)
+            productToEdit.save()
+            res.redirect("/products")
+        },
+        "error": async(form)=> {
+            res.render("products/update", {
+                "form": form.toHTML(bootstrapField)
+            })
+        }
+    })
+})
+
+
 module.exports = router
