e84585f0a3176e2e41f95a9598ce1b04f68c9a55
yongsannnn <yong_s@live.com>
2021-03-25 07:56:14 +0000

    removed env

--- route/cloudinary.js
+++ route/cloudinary.js
@@ -6,9 +6,8 @@ const cloudinary = require("cloudinary")
 router.get("/sign", async (req,res)=>{
     //retrieve the parameter we need to send to cloudinary
     const params_to_sign = JSON.parse(req.query.params_to_sign);
-
     // retrieve our cloudinary api secret from our environment
-    const apiSecret = process.env.CLOUDINARY_API_KEY
+    const apiSecret = process.env.CLOUDINARY_API_SECRET
 
     // get the signature (CSRF)
     const signature = cloudinary.utils.api_sign_request(params_to_sign,apiSecret)
